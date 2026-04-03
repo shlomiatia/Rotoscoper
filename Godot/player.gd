50888d78f08b5f5c6e8eeed387ae78297cf0bbb8
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-04-03 16:21:04 +0300

    aaa

--- Godot/player.gd
+++ Godot/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = 300.0
 
 enum State {
     STAND,
@@ -12,7 +12,10 @@ enum State {
     TURN,
     JUMP_START,
     JUMP,
-    JUMP_END
+    JUMP_END,
+    JUMP_FORWARD_START,
+    JUMP_FORWARD,
+    JUMP_FORWARD_END
 }
 
 var current_state: State = State.STAND
@@ -30,15 +33,23 @@ func _physics_process(delta: float) -> void:
 
     # Input
     var direction := Input.get_axis("ui_left", "ui_right")
-    if Input.is_action_just_pressed("ui_accept") and current_state == State.STAND:
-        change_state(State.JUMP_START)
-
+    if current_state == State.STAND:
+        if Input.is_action_just_pressed("ui_accept"):
+            change_state(State.JUMP_START)
+    if current_state == State.START_WALK || current_state == State.WALK:
+        if Input.is_action_just_pressed("ui_accept") && direction_match_orientation(direction):
+            change_state(State.JUMP_FORWARD_START)
+    if current_state == State.JUMP_START:
+        if direction_match_orientation(direction):
+            change_state(State.JUMP_FORWARD_START)
+    
+                
     # State logic
     match current_state:
         State.STAND:
             velocity.x = direction * SPEED if direction != 0 else move_toward(velocity.x, 0, SPEED)
             if direction != 0:
-                if animated_sprite_2d.flip_h != (direction < 0):
+                if not direction_match_orientation(direction):
                     # Direction opposite - need to turn
                     change_state(State.TURN)
                 else:
@@ -50,27 +61,31 @@ func _physics_process(delta: float) -> void:
         State.WALK:
             if direction == 0:
                 change_state(State.STAND)
-            elif animated_sprite_2d.flip_h != (direction < 0):
+            elif not direction_match_orientation(direction):
                 # Direction opposite - need to turn
                 change_state(State.TURN)
             else:
                 velocity.x = direction * SPEED
-        State.JUMP_START:
-            pass # Wait for animation
         State.JUMP:
             if just_entered_jump:
                 just_entered_jump = false
             elif is_on_floor():
                 change_state(State.JUMP_END)
-        State.JUMP_END:
-            pass # Wait for animation
+        State.JUMP_FORWARD:
+            if just_entered_jump:
+                just_entered_jump = false
+            elif is_on_floor():
+                change_state(State.JUMP_FORWARD_END)
 
     move_and_slide()
 
+func direction_match_orientation(direction: float) -> bool:
+    if animated_sprite_2d.flip_h:
+        return direction < 0
+    else:
+        return direction > 0
+
 func change_state(new_state: State) -> void:
-    # Exit current state if needed
-    # (none for now)
-    # Enter new state
     current_state = new_state
     match current_state:
         State.STAND:
@@ -92,12 +107,28 @@ func change_state(new_state: State) -> void:
             _set_sprite_position(Vector2(3.5, -50.5))
         State.JUMP:
             animated_sprite_2d.play("jump")
-            velocity.y = JUMP_VELOCITY
+            velocity.y = - JUMP_VELOCITY
             just_entered_jump = true
             _set_sprite_position(Vector2(3.5, -50.5))
         State.JUMP_END:
             animated_sprite_2d.play_backwards("jump_start")
             _set_sprite_position(Vector2(3.5, -50.5))
+        State.JUMP_FORWARD_START:
+            velocity.x = 0
+            animated_sprite_2d.play("jump_forward_start")
+            _set_sprite_position(Vector2(22, -38))
+        State.JUMP_FORWARD:
+            animated_sprite_2d.play("jump_forward")
+            if animated_sprite_2d.flip_h:
+                velocity = Vector2(-2, -1).normalized() * 200
+            else:
+                velocity = Vector2(2, -1).normalized() * 200
+            just_entered_jump = true
+            _set_sprite_position(Vector2(22, -38))
+        State.JUMP_FORWARD_END:
+            animated_sprite_2d.play("jump_forward_end")
+            velocity.x = 0
+            _set_sprite_position(Vector2(22, -38))
 
 func _set_sprite_position(base_position: Vector2) -> void:
     var pos := base_position
@@ -108,14 +139,20 @@ func _set_sprite_position(base_position: Vector2) -> void:
 func _on_AnimatedSprite2D_animation_finished() -> void:
     match current_state:
         State.START_WALK:
-            if animated_sprite_2d.animation == "start_walk":
-                change_state(State.WALK)
+            change_state(State.WALK)
         State.TURN:
             animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
             change_state(State.STAND)
         State.JUMP_START:
-            if animated_sprite_2d.animation == "jump_start":
-                change_state(State.JUMP)
+            change_state(State.JUMP)
         State.JUMP_END:
-            if animated_sprite_2d.animation == "jump_start":
-                change_state(State.STAND)
+            change_state(State.STAND)
+        State.JUMP_FORWARD_START:
+            change_state(State.JUMP_FORWARD)
+        State.JUMP_FORWARD_END:
+            if animated_sprite_2d.flip_h:
+                position.x -= 54
+            else:
+                position.x += 54
+
+            change_state(State.STAND)
